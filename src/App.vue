fa5ca631f2edca262ec76036510e58c577ef4af3
antonio-martins-sh3 <ac.martins@sh3.com>
2024-12-13 16:49:34 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup></script>
+
+<template>
+  <div id="app">
+    <router-view />
+    <!-- Where the current view will be rendered -->
+  </div>
+</template>
+
+<style scoped>
+pokedex {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 24px;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
